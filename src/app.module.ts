a78b0b3a0c08e9a0c17f879df9c601582a5f52a3
Yoppy <yoppylands@gmail.com>
2022-01-18 10:49:09 +0100

    change env setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: `.env.production`,
     }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
